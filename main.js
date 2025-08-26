27f381e6283eec4c7e897d6fe77b1f64a91c0436
Daniel Ciferni <cif.daniel96@gmail.com>
2025-08-26 16:42:47 +0200

    Terminato Selfwork cicli 3

--- main.js
+++ main.js
@@ -1,28 +1,20 @@
-let bevanda = Number(prompt(`Ciao, quale bevanda desideri? Inserisci il numero corrispondente alla bevanda:\n1=Acqua\n2=Coca cola\n3=Birra`));
-
-
-while (bevanda < 1 || bevanda > 3) {
-    bevanda = Number(prompt(`Scelta non valida.\nCiao, quale bevanda desideri? Inserisci il numero corrispondente alla bevanda:\n1=Acqua\n2=Coca cola\n3=Birra`));
-}
-if (bevanda == 1) {
-    console.log(`Ecco qui la tua Acqua`);
-} else if (bevanda == 2) {
-    console.log(`Ecco la tua Coca cola`);
-} else if (bevanda == 3) {
-    console.log(`Ecco la tua Birra`);
-}
-
-// Extra
-
-let bevandaExtra = prompt(`Ciao, quale bevanda desideri? Inserisci il numero corrispondente alla bevanda:\n1=Acqua\n2=Coca cola\n3=Birra`);
-
-while (bevandaExtra !== `1` && bevandaExtra !== `2` && bevandaExtra !== `3` && bevandaExtra !== `Acqua` && bevandaExtra !== `Coca cola` && bevandaExtra !== `Birra`) {
-    bevandaExtra = prompt(`Scelta non valida.\nCiao, quale bevanda desideri? Inserisci il numero corrispondente alla bevanda:\n1=Acqua\n2=Coca cola\n3=Birra`);
-}
-if (bevandaExtra == `1` || bevandaExtra == `Acqua`) {
-    console.log(`Ecco qui la tua Acqua`);
-} else if (bevandaExtra == `2` || bevandaExtra == `Coca cola`) {
-    console.log(`Ecco la tua Coca cola`);
-} else if (bevandaExtra == `3` || bevandaExtra == `Birra`) {
-    console.log(`Ecco la tua Birra`);
-}
+let bevanda;
+do {
+    bevanda = Number(prompt(`Ciao, quale bevanda desideri? Inserisci il numero corrispondente alla bevanda:\n1=Acqua\n2=Coca cola\n3=Birra`));
+    switch (bevanda) {
+        case 1:
+            console.log(`E’ stata selezionata l’Acqua`);
+            break;
+        case 2:
+            console.log(`E’ stata selezionata la Coca cola`);
+            break;
+        case 3:
+            console.log(`E’ stata selezionata la Birra`);
+            break;
+        case typeof bevanda !== `number`:
+            console.log(`Scelta non valida, per favore riprova. Ciao, quale bevanda desideri?`);
+            break;
+        default:
+            console.log(`Scelta non valida, per favore riprova. Ciao, quale bevanda desideri?`);
+    }
+} while (bevanda !== 1 && bevanda !== 2 && bevanda !== 3);
